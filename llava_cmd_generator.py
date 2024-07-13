ea8a9ab473c794d914019d9aea275f957b885927
VINAY PRITHYANI <vinayprithyani@vinays-mbp.lan>
2024-07-13 13:47:03 -0400

    wrapper script to automate fine-tuning

---
+++ llava_cmd_generator.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+def generate_llava_finetune_command( *,
+                                     llava_root,
+                                     train_file,
+                                     dataset_dir,
+                                     checkpoint_name,
+                                     num_epochs,
+                                     context_length,
+                                     validation_file
+                                   ):
+    llava_cmd = f"""
+deepspeed {llava_root}/llava/train/train_mem.py \
+--lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
+--deepspeed {llava_root}/scripts/zero3.json \
+--model_name_or_path liuhaotian/llava-v1.5-7b \
+--version v1 \
+--data_path {train_file} \
+--image_folder {dataset_dir} \
+--vision_tower openai/clip-vit-large-patch14-336 \
+--mm_projector_type mlp2x_gelu \
+--mm_vision_select_layer -2 \
+--mm_use_im_start_end False \
+--mm_use_im_patch_token False \
+--image_aspect_ratio pad \
+--group_by_modality_length True \
+--bf16 True \
+--output_dir {llava_root}/checkpoints/llava-v1.5-7b-task-lora_{checkpoint_name} \
+--num_train_epochs {num_epochs} \
+--per_device_train_batch_size 4 \
+--per_device_eval_batch_size 1 \
+--gradient_accumulation_steps 4 \
+--evaluation_strategy "no" \
+--save_strategy "epoch" \
+--save_total_limit 1 \
+--learning_rate 2e-4 \
+--weight_decay 0. \
+--warmup_ratio 0.03 \
+--lr_scheduler_type "cosine" \
+--logging_steps 1 \
+--tf32 True \
+--model_max_length {context_length} \
+--gradient_checkpointing True \
+--dataloader_num_workers 4 \
+--lazy_preprocess True \
+--report_to tensorboard \
+--validation_data_path {validation_file} \
+--evaluation_strategy "steps" \
+--report_to wandb"""
+    return llava_cmd
---
+++ train.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+
+import argparse
+import yaml
+import os
+import subprocess
+import sys
+
+from llava_cmd_generator import generate_llava_finetune_command
+
+'''
+Different experiments to run
+
+1. Halve all downsamples, and double all context lengths for relevant datasets
+2. Halve all downsamples, double context lengths, but also downsample the image resolution itself
+3. Change dataset.py to beef up the conversations
+4. Change dataset.py to generate differnt kinds of images
+5. Fix the multivaraite TSC datasets to use here
+
+'''
+
+SUPPORTED_DATASETS = ["TwoLeadECG", "CinCECGTorso", "ItalyPowerDemand", "FreezerSmallTrain", "PenDigits", "PhalangesOutlinesCorrect", "HandOutlines" ]
+
+def finetune( *, downsample, round_to, dataset, vlm_root, llava_root, num_epochs, context_length):
+
+    ####### Generate configs #########################
+    config_template = os.path.join(vlm_root, "configs/llava_config.yaml")
+    assert os.path.exists(config_template)
+    with open(config_template, "r") as fh:
+        config = yaml.safe_load(fh)
+    print(config)
+    config["dataset"]["name"] = f'{dataset}'
+
+    llava_data_dir = f"{llava_root}/playground/new_data/"
+    os.system(f"mkdir -p {llava_data_dir}")
+
+    llava_dataset_dir = f"{llava_data_dir}/{dataset}_downsample_{downsample}_round_{round_to}"
+    os.system(f"mkdir -p {llava_dataset_dir}")
+    config["data_path"] = llava_dataset_dir
+
+    llava_image_dir = f"{llava_dataset_dir}/images"
+    os.system(f"mkdir -p {llava_image_dir}")
+    config["image_path"] = f"{llava_image_dir}"
+    config["options"]["round_to"] = round_to
+    config["options"]["downsample_to"] = downsample if downsample > 0 else None
+
+    config_filename = f"{vlm_root}/configs/llava_config_{dataset}_downsample_{downsample}_round_{round_to}.yaml"
+    with open(config_filename, "w") as f:
+        yaml.dump(config, f)
+    ###### Run dataset.py to generate data ##################
+    dataset_file = f"{vlm_root}/dataset.py"
+    assert os.path.exists(dataset_file)
+    #Check if we already generated data
+    train_file = f"{llava_dataset_dir}/train.json"
+    if not os.path.exists(train_file):
+        data_generation_cmd = f"python {dataset_file} {config_filename}"
+        cp = subprocess.run(data_generation_cmd.split(), capture_output=True)
+        cp.check_returncode()
+    #After this step train_file should exist
+    assert(os.path.exists(train_file))
+
+    #### Run llava fine-tuning
+    llava_finetune_cmd = generate_llava_finetune_command(
+        llava_root = llava_root,
+        train_file = train_file,
+        dataset_dir = llava_dataset_dir,
+        checkpoint_name = f'{dataset}_downsample_{downsample}_round_{round_to}',
+        num_epochs = num_epochs,
+        context_length = context_length,
+        validation_file = f'{llava_dataset_dir}/validation.json'
+    )
+    print(llava_finetune_cmd)
+
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("--downsample", type=int, required=True)
+    argparser.add_argument("--round_to", type=int, default=True)
+    argparser.add_argument("--dataset", type=str, required=True, choices=SUPPORTED_DATASETS)
+    argparser.add_argument("--vlm-root", type=str, required=True, help="Root Dir for VLM-TSC git repo")
+    argparser.add_argument("--llava-root",type=str,required=True, help="Root Dir for LLaVA git repo")
+    argparser.add_argument("--num-epochs", type=int, default=2)
+    argparser.add_argument("--context-length", type=int, default=2048)
+    args = argparser.parse_args()
+    finetune( downsample=args.downsample,
+              round_to=args.round_to,
+              dataset=args.dataset,
+              vlm_root=args.vlm_root,
+              llava_root=args.llava_root,
+              num_epochs = args.num_epochs,
+              context_length = args.context_length
+            )
